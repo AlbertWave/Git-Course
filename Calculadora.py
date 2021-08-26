db69b9f3190a0483a44826d1e386e76c4ac610e7
Eduardht <eduardht.sv@gmail.com>
2021-08-26 18:24:26 -0500

    Primer commit

---
+++ Calculadora.py
@@ -0,0 +1,10 @@
+
+
+def sumar(op1, op2):
+    return op1 + op2
+
+
+numero1 = int(input("Por favor introduce un valor: "))
+numero2 = int(input("Por favor introduce un valor: "))
+print("el resultado de la suma es: ", sumar(numero1, numero2))
+
