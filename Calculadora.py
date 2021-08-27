b1d300bf7877b023dd8aa18472e835a9e200d44a
Eduardht <eduardht.sv@gmail.com>
2021-08-27 02:24:05 -0500

    Funciones de resta y multiplicacion.

--- Calculadora.py
+++ Calculadora.py
@@ -3,8 +3,12 @@
 def sumar(op1, op2):
     return op1 + op2
 
+def sumar(op1, op2):
+    return op1 - op2
+
+def sumar(op1, op2):
+    return op1 * op2
 
 numero1 = int(input("Por favor introduce un valor: "))
 numero2 = int(input("Por favor introduce un valor: "))
 print("el resultado de la suma es: ", sumar(numero1, numero2))
-
